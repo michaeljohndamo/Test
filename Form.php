fb5cbd00b619a17f3e2e13a2153ff9e4f8fdbf5f
Michael John Damo <michaeljohndamo@Michaels-MacBook-Pro.local>
2016-04-28 17:53:32 +0800

    second commit

---
+++ Form.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html> 
+	<head> 
+		<title> Php form </title>
+	</head>
+	<body> 
+		<form action="index.php"> 
+			<table> 
+				<tr> 
+					<td>Username:</td>
+					<td><input type="email" name="username"></td>
+				</tr>
+				<tr> 
+					<td>Password:</td>
+					<td><input type="password" name="password"></td>
+				</tr>
+				<tr> 
+					<td> </td>
+					<td><input type="submit" name="submit"></td>
+				</tr>
+			</table>
+
+
+		</form>
+	</body>
+</html>
