ba89fd1711d0f08c3f7b16acbfa4c05de6ba7aa4
Michael John Damo <michael.damo@autodeal.com.ph>
2016-05-01 23:53:30 +0800

    8th commit

---
+++ form_process.php
@@ -0,0 +1,27 @@
+<?php 
+include "includes/db.php";
+if(isset($_POST['submit_form'])){
+	$name = $_POST['name'];
+	$email=	$_POST['email'];
+	$subject = $_POST['subject'];
+	$gender =$_POST['gender'];
+	$country = $_POST['country'];
+	$comments = $_POST['comments'];
+if(empty($_POST['skills1'])) {
+	$_POST['skills1'] = "";
+}
+if(empty($_POST['skills2'])) {
+	$_POST['skills2'] = "";
+}
+if(empty($_POST['skills3'])) {
+	$_POST['skills3'] = "";
+}
+if(empty($_POST['skills4'])) {
+	$_POST['skills4'] = "";
+}
+
+$ins_sql = "INSERT INTO php_course (name, email, subject, gender, skills1, skills2, skills3, skills4, country, comments) VALUES ('$name', '$email', '$subject', '$gender', '$_POST[skills1]', '$_POST[skills2]', '$_POST[skills3]','$_POST[skills4]', '$country', '$comments')";
+$run_sql = mysqli_query($conn,$ins_sql);
+}
+
+?>
