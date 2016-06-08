e8fb18cb6c1677082a6c11e0706be47e7a31dd28
Michael John Damo <michaeljohndamo@Michaels-MacBook-Pro.local>
2016-06-08 18:50:30 +0800

    cms

---
+++ cms/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+	<head> 
+		<title>NoypiNews2</title>
+		<link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+		<script src="../bootstrap/js/bootstrap.js"></script>
+		<script src="../js/jquery.js"></script>
+	</head>
+	<body> 
+			<header class="navbar navbar-inverse navbar-static-top">
+					<div class="container">
+						<a href="index.php" class="navbar-brand">Jay Damo</a>
+						<ul class="nav navbar-nav navbar-right"> 
+							<li><a href="#">Home</a></li>
+							<li><a href="#">Articles</a></li>
+							<li><a href="#">Contact Us</a></li>
+							<li><a href="#">Log Out</a></li>
+						</ul>
+					</div>
+			 </header>
+			 <div class="container">
+			 	<article class="row"> 
+			 		<section class="col-lg-8">
+			 			<div class="panel panel-default"> 
+			 				<div class="panel-body">
+			 					<div class="panel-header"> 
+			 						<h3>Mitsubishi Lancer GLXi 93</h3>
+			 					</div>
+			 					<img src="images/lancer93.jpg" width="100%">
+			 					<p>Very nice interior, though the sidings are plastic. Too much vibration creates noise from the plastic interior.
+It has cool A/C, nice flooring and ceilings.
+This car with the 4g92 stock engine has reached 180 kph on a freeway.
+The money I spent for all enhancements I have applied to this car is worth it. HKS grounding kit, KYB gas shock absorbers, HID light, Splitfire spark plugs and Simota air filter. That stuffs is worth it! Performance wise, I can't complain! </p>
+			 				</div>
+			 		</section>
+			 	<form class="panel-group form-horizontal "role="form">
+			 		<aside class="col-lg-4"> 
+			 			<div class="panel panel-default"> 
+			 				<div class="panel-heading">Login Area</div>
+			 				<div class="panel-body"> 
+			 					<div class="form-group">
+			 					<label for="username" class="control-label col-sm-4">Username</label>
+			 					<div class="col-sm-8">
+			 						<input type="text" id="username" class="form-control" placeholder="Username/Email address">
+			 					</div>
+			 					</div>
+			 					<div class="form-group">
+			 					<label for="username" class="control-label col-sm-4">Password</label>
+			 					<div class="col-sm-8">
+			 						<input type="password" id="password" class="form-control" placeholder="Password">
+			 					</div>
+			 					</div>
+			 				</div>
+			 			</div>
+			 		</aside>
+			 	</form>	
+			 	</article>
+			 </div>
+	</body>
+</html
